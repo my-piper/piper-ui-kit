049c7c4ed3740593aadd693a95e6e3ffcdce42ae
breslavsky <breslavsky.anton@gmail.com>
2025-02-23 18:08:53 +0100

    Fix ui kit

--- ui-kit.module.ts
+++ ui-kit.module.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
-<<<<<<< HEAD
 import { IncludesPipe } from "src/ui-kit/pipes/includes.pipe";
-=======
 import { InViewportDirective } from "./directives/in-viewport.directive";
->>>>>>> b3a9132ee0aeb8857712eb091b210fc3c9e92d8e
 import { IconComponent } from "./icon/icon.component";
 import { InformerComponent } from "./informer/informer.component";
 import { ModalComponent } from "./modal/modal.component";
@@ -39,11 +36,8 @@ import { TabsComponent } from "./tabs/tabs.component";
     DistancePipe,
     CountdownPipe,
     I18nPipe,
-<<<<<<< HEAD
     IncludesPipe,
-=======
     InViewportDirective,
->>>>>>> b3a9132ee0aeb8857712eb091b210fc3c9e92d8e
   ],
   exports: [
     IconComponent,
@@ -60,11 +54,8 @@ import { TabsComponent } from "./tabs/tabs.component";
     DistancePipe,
     CountdownPipe,
     I18nPipe,
-<<<<<<< HEAD
     IncludesPipe,
-=======
     InViewportDirective,
->>>>>>> b3a9132ee0aeb8857712eb091b210fc3c9e92d8e
   ],
   imports: [CommonModule, ReactiveFormsModule],
 })
