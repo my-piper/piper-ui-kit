93e696aeb1dcd75d65df19e362719a0a5732fbec
breslavsky <breslavsky.anton@gmail.com>
2025-03-06 23:10:02 +0100

    fix(ui): countdown

--- pipes/countdown.pipe.ts
+++ pipes/countdown.pipe.ts
@@ -3,31 +3,19 @@ import { BehaviorSubject, Observable } from "rxjs";
 
 @Pipe({ name: "countdown" })
 export class CountdownPipe implements PipeTransform {
-  prev: number = 0;
   timer: any | null = null;
 
   transform(number: number): Observable<number> {
     clearTimeout(this.timer);
-    this.prev = 0;
-    const direction: "up" | "down" = number > this.prev ? "up" : "down";
-    let current = this.prev;
-
-    const step = number % 1 !== 0 ? number / 100 : 1;
+    let current = number / 5;
 
     const counter = new BehaviorSubject<number>(current);
     const count = () => {
-      current += Math.max(
-        number / 5,
-        (Math.abs(number - current) / 2) * (direction === "up" ? step : -step)
-      );
-      counter.next(current);
-      if (
-        (direction === "up" && current < number) ||
-        (direction === "down" && current > number)
-      ) {
+      current += Math.max((number - current) / 4, number > 0 ? 0.005 : -0.005);
+      if (number > 0 ? current < number : current > number) {
+        counter.next(current);
         this.timer = setTimeout(count, 50);
       } else {
-        this.prev = number;
         counter.next(number);
       }
     };
