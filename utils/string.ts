ba4bceb6db88e446d78e6a0da9afaae0d511d04e
breslavsky <breslavsky.anton@gmail.com>
2025-04-06 00:40:19 +0200

    feat: add length to sid

--- utils/string.ts
+++ utils/string.ts
@@ -1,5 +1,5 @@
 import { customAlphabet } from "nanoid";
 
-export function sid(): string {
-  return customAlphabet("1234567890abcdef", 10)();
+export function sid(length: number = 10): string {
+  return customAlphabet("1234567890abcdef", length)();
 }
